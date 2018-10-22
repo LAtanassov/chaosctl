5987feca3083469f35fc7888ba102e46d0e67d42
latanassov <latschesar.atanassov@gmx.at>
2018-10-22 18:46:38 +0200

    ci-script update

--- ci-script.sh
+++ ci-script.sh
@@ -1,15 +1,14 @@
-#!/bin/bash -e
-# used for development
+#!/bin/sh -e
 
-GO_FILES=$(find . -iname '*.go' -type f | grep -v /vendor/) # All the .go files, excluding vendor/
-# go get golang.org/x/vgo
-go get github.com/golang/lint/golint                        # Linter
-go get honnef.co/go/tools/cmd/megacheck                     # Badass static analyzer/linter
-go get github.com/fzipp/gocyclo
+GO_FILES=$(find . -iname '*.go' -type f | grep -v /vendor/)
 
-test -z $(gofmt -s -l $GO_FILES)         # Fail if a .go file hasn't been formatted with gofmt
-vgo test -v -race $GO_FILES                   # Run all the tests with the race detector enabled
-vgo vet $GO_FILES                             # go vet is the official Go static analyzer
-megacheck ./...                          # "go vet on steroids" + linter
-gocyclo -over 19 $GO_FILES               # forbid code with huge functions
-golint -set_exit_status $(go list ./...) # one last linter
+test -z $(go fmt ./...)
+
+go test -v -race ./...
+go vet ./...
+
+#megacheck ./...
+gocyclo -over 19 $GO_FILES
+golint -set_exit_status $(go list ./...)
+
+go build -tags netgo ./...
